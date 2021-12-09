4ebd5e9c9b8db16ffe12ee9fd51ad9afd7496819
lorenz.beyer@orca-software.com <lorenz.beyer@orca-software.com>
2021-12-09 13:49:03 +0100

    update cookiebot id

--- src/app/cookiebot.config.ts
+++ src/app/cookiebot.config.ts
@@ -2,7 +2,7 @@ import { NgxCookiebotConfig } from '@halloverden/ngx-cookiebot';
 
 export class CookiebotConfig extends NgxCookiebotConfig {
   blockingMode: 'auto' | 'manual' | string = 'auto';
-  cbId: string = '3745138d-12b2-427c-ad18-9e3cdd022d0e';
+  cbId: string = 'af590895-94ce-4b67-a09c-d29c65d2f2e5';
   culture?: string;
   framework?: string;
   level?: string;
